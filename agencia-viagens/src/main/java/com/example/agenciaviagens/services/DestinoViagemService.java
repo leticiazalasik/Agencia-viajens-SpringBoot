aa76d31578621e1ac59039503d2a486de42d7007
saviozoboli <savio.zoboli@gmail.com>
2026-05-20 19:02:59 -0300

    feat: criado classe da service e os métodos vazios

---
+++ agencia-viagens/src/main/java/com/example/agenciaviagens/services/DestinoViagemService.java
@@ -0,0 +1,45 @@
+package com.example.agenciaviagens.services;
+
+import com.example.agenciaviagens.entity.Destino;
+import com.example.agenciaviagens.entity.DestinoResumo;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class DestinoViagemService{
+
+    public List<DestinoResumo> listarComFiltro(Map<String,String> filtro){
+
+
+        return null;
+    }
+
+    public Destino criarDestino(Destino destino){
+
+        return null;
+    }
+
+
+    public Destino buscarDestinoPorId(Long id){
+
+
+        return null;
+    }
+
+    public Destino atualizarDestino(Long id,Destino destino){
+
+        return null;
+    }
+
+    public void excluirDestino(Long id){
+
+    }
+
+
+
+
+
+
+
+}
